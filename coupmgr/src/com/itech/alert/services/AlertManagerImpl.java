ef9087944ddeff79a677b87c017773d3dca19a0a
dadua <dadua@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2012-04-21 10:59:12 +0000

    beans.xml updated for alerts services.

--- coupmgr/src/com/itech/alert/services/AlertManagerImpl.java
+++ coupmgr/src/com/itech/alert/services/AlertManagerImpl.java
@@ -2,10 +2,12 @@ package com.itech.alert.services;
 
 import java.util.List;
 
+import com.itech.alert.dao.AlertDAO;
 import com.itech.alert.model.Alert;
 import com.itech.coupon.model.User;
 
 public class AlertManagerImpl implements AlertManager {
+	private AlertDAO alertDAO;
 
 	@Override
 	public void delete(Alert alert) {
@@ -35,6 +37,14 @@ public class AlertManagerImpl implements AlertManager {
 	public void save(Alert alert) {
 		// TODO Auto-generated method stub
 
+	}
+
+	public void setAlertDAO(AlertDAO alertDAO) {
+		this.alertDAO = alertDAO;
+	}
+
+	public AlertDAO getAlertDAO() {
+		return alertDAO;
 	}
 
 }
