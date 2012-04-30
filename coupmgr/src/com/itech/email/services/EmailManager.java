c6828ff5ab902774832b5285cb164536c33c81be
bpsingh <bpsingh@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2012-04-30 18:11:01 +0000

    adding html , attachement with email and UT

--- coupmgr/src/com/itech/email/services/EmailManager.java
+++ coupmgr/src/com/itech/email/services/EmailManager.java
@@ -1,5 +1,8 @@
 package com.itech.email.services;
 
+import java.io.File;
+import java.util.List;
+
 public interface EmailManager {
 	/**
 	 * Send Email ; config taken from beans.xml for now
@@ -7,6 +10,17 @@ public interface EmailManager {
 	 * @param to to email address
 	 * @param subject
 	 * @param msg
+	 * @return
 	 */
-	public void sendMail(String from, String to, String subject, String msg);
+	public boolean sendMail(String from, String to, String subject, String msg);
+	/**
+	 * end Email with attachements; config taken from beans.xml for now
+	 * @param from from email address
+	 * @param to to email address
+	 * @param subject subject
+	 * @param htmlMsg
+	 * @param attachmentFiles list of Files() to be attached
+	 * @return
+	 */
+	public boolean sendMailWithAttachement(String from, String to, String subject, String htmlMsg, List<File> attachmentFiles);
 }
--- coupmgr/src/com/itech/email/services/EmailManagerImpl.java
+++ coupmgr/src/com/itech/email/services/EmailManagerImpl.java
@@ -1,24 +1,75 @@
 package com.itech.email.services;
 
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
+import java.io.File;
+import java.util.List;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.apache.log4j.Logger;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
 
 public class EmailManagerImpl implements EmailManager {
-	private MailSender mailSender;
-
-	public void setMailSender(MailSender mailSender) {
+	private JavaMailSenderImpl mailSender;
+	Logger logger = Logger.getLogger(EmailManagerImpl.class);
+	public void setMailSender(JavaMailSenderImpl mailSender) {
 		this.mailSender = mailSender;
 	}
 
-	public void sendMail(String from, String to, String subject, String msg) {
+	@Override
+	public boolean sendMail(String from, String to, String subject, String htmlMsg) {
+		try{
+			MimeMessage message = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message);
+			setMailContent(helper, from, to, subject, htmlMsg);
+			mailSender.send(message);
+		}catch(MessagingException me){
+			logger.debug("Email sending failed for mail:  from ["+ from +"] ; to ["+to+"]" );
+			return false;
+		}
+		return true;
+	}
 
-		SimpleMailMessage message = new SimpleMailMessage();
 
-		message.setFrom(from);
-		message.setTo(to);
-		message.setSubject(subject);
-		message.setText(msg);
-		mailSender.send(message);
+	@Override
+	public boolean sendMailWithAttachement(String from, String to, String subject, String htmlMsg, List<File> attachmentFiles) {
+		try{
+			MimeMessage message = mailSender.createMimeMessage();
+			MimeMessageHelper helper = null;
+			helper = getAttachementMineMessageHelper(message);
+			setMailContent(helper, from, to, subject, htmlMsg);
+			addAttachements(helper, attachmentFiles);
+			mailSender.send(message);
+		}catch(MessagingException me){
+			logger.debug("Email sending failed for mail:  from ["+ from +"] ; to ["+to+"]" );
+			return false;
+		}
+		return true;
+	}
+
+	private MimeMessageHelper getAttachementMineMessageHelper (
+			MimeMessage message) throws MessagingException{
+		MimeMessageHelper mimeMessageHelper = null;
+		mimeMessageHelper = new MimeMessageHelper(message, true);
+		return mimeMessageHelper;
+	}
+
+	private void addAttachements(MimeMessageHelper helper,
+			List<File> attachmentFiles) throws MessagingException{
+		for(File file : attachmentFiles){
+			FileSystemResource attfile = new FileSystemResource(file);
+			helper.addAttachment(file.getName(), attfile);
+		}
+	}
+
+	private void setMailContent(MimeMessageHelper helper, String from, String to, String subject, String htmlMsg) throws MessagingException{
+		helper.setFrom(from);
+		helper.setTo(to);
+		helper.setSubject(subject);
+		helper.setText(htmlMsg, true);
 	}
 }
 
