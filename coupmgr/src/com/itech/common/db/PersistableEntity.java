c4c50f9e01bbc1b7b4507a7ce662cf8635f62fff
dadua <dadua@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2012-05-12 07:29:48 +0000

    Mapping updated

--- coupmgr/src/com/itech/common/db/PersistableEntity.java
+++ coupmgr/src/com/itech/common/db/PersistableEntity.java
@@ -1,30 +1,8 @@
 package com.itech.common.db;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 public abstract class PersistableEntity {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="ID")
-	private Long id;
-
-	public boolean isTransient() {
-		if (getId() == null) {
-			return true;
-		}
-		return false;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getId() {
-		return id;
-	}
+	public abstract boolean isTransient();
 
 }
--- coupmgr/src/com/itech/offer/model/OfferBankCardAssoc.java
+++ coupmgr/src/com/itech/offer/model/OfferBankCardAssoc.java
@@ -1,8 +1,54 @@
 package com.itech.offer.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import com.itech.common.db.PersistableEntity;
 
+@Entity
+@Table(name="OFFER_CARD_ASSOC")
 public class OfferBankCardAssoc extends PersistableEntity{
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name="OFFER_ID")
 	private Offer offer;
+
+	@ManyToOne
+	@JoinColumn(name="BANK_CARD_ID")
 	private BankCard bankCard;
+
+	public Offer getOffer() {
+		return offer;
+	}
+	public void setOffer(Offer offer) {
+		this.offer = offer;
+	}
+	public BankCard getBankCard() {
+		return bankCard;
+	}
+	public void setBankCard(BankCard bankCard) {
+		this.bankCard = bankCard;
+	}
+	@Override
+	public boolean isTransient() {
+		return id == null;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getId() {
+		return id;
+	}
+
 }
