893044503056a21000bc79362916fea50cd48506
aloksharma <aloksharma@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2012-07-20 21:03:36 +0000

    o Autocomplete functionality for vendors is now working.. o Fixing some of the incorrect checkin code when rename of coupon to offer was done

--- coupmgr/src/com/itech/coupon/OfferWalletConstants.java
+++ coupmgr/src/com/itech/coupon/OfferWalletConstants.java
@@ -6,4 +6,12 @@ public class OfferWalletConstants {
 	public static final String WALLET_PAGE = "wallet.jsp";
 	public static final String INDEX_PAGE = "index.jsp";
 	public static final String OFFER_NOTIFICATION_CONFIG_PARAM_KEY = "offerNotificationConfigs";
+
+
+
+	public static final String GET_SHARED_OFFER_PAGE = "shared-offer.jsp";
+	public static final String SHARED_OFFER_ACCESS_CODE_PARAM_KEY = "access-code";
+	public static final String SHARED_OFFER_ATTR_KEY = "sharedOffer";
+	public static final String SHARED_OFFER_URL_ATTR_KEY = "sharedOfferUrl";
+	public static final String SHARED_OFFER_ID_ATTR_KEY = "sharedOfferId";
 }
