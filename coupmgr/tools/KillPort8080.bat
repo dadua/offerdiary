df825c52480ceb71373835b787e9827158dbcf14
aloksharma <aloksharma@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2012-07-21 05:29:10 +0000

    corrections

--- coupmgr/tools/KillPort8080.bat
+++ coupmgr/tools/KillPort8080.bat
@@ -2,7 +2,7 @@
 
 SETLOCAL
 
-if "X%1" == "X" ( set PORT_TO_BE_CHECKED=8080) else (set PORT_TO_BE_CHECKED=8080)
+if "X%1" == "X" ( set PORT_TO_BE_CHECKED=8080) else (set PORT_TO_BE_CHECKED=%1)
 
 for /f "tokens=1,4,5" %%i in ('netstat -ano ^| findstr 8080') do if "X%%j" == "XLISTENING" ( set PID_WHICH_HAS_PORT_8080=%%k )
 
