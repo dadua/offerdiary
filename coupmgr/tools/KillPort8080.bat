518236b9ac83ccb9b4f9d7c925ff16e3914025a1
aloksharma <aloksharma@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2012-07-21 02:20:11 +0000

    kill process listening on 8080

---
+++ coupmgr/tools/KillPort8080.bat
@@ -0,0 +1,14 @@
+@echo off
+
+SETLOCAL
+
+set PORT_TO_BE_CHECKED=8080
+
+for /f "tokens=1,4,5" %%i in ('netstat -ano ^| findstr 8080') do if "X%%j" == "XLISTENING" ( set PID_WHICH_HAS_PORT_8080=%%k )
+
+if "X%PID_WHICH_HAS_PORT_8080%" == "X" (
+    echo No Process listening on port 8080
+    exit /b -1
+    )
+echo %PID_WHICH_HAS_PORT_8080%
+taskkill /f /PID %PID_WHICH_HAS_PORT_8080%
