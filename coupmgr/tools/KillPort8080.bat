4d69d71762e40bcfd26ecbaf568af097292d8943
aloksharma <aloksharma@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2012-07-21 07:11:25 +0000

    Minute ui modifications

--- coupmgr/tools/KillPort8080.bat
+++ coupmgr/tools/KillPort8080.bat
@@ -4,7 +4,7 @@ SETLOCAL
 
 if "X%1" == "X" ( set PORT_TO_BE_CHECKED=8080) else (set PORT_TO_BE_CHECKED=%1)
 
-for /f "tokens=1,4,5" %%i in ('netstat -ano ^| findstr 8080') do if "X%%j" == "XLISTENING" ( set PID_WHICH_HAS_PORT_8080=%%k )
+for /f "tokens=1,4,5" %%i in ('netstat -ano ^| findstr %PORT_TO_BE_CHECKED%') do if "X%%j" == "XLISTENING" ( set PID_WHICH_HAS_PORT_8080=%%k )
 
 if "X%PID_WHICH_HAS_PORT_8080%" == "X" (
     echo No Process listening on port 8080
