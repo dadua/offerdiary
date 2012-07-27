7ad37ab5f9ad100d51be9fcf6d32164eb7d63bf1
bpsingh <bpsingh@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2012-07-27 21:49:49 +0000

    adding red wine parser , minor fixes needed

---
+++ coupmgr/src/com/itech/redwine/parser/RedWineCardsParser.java
@@ -0,0 +1,132 @@
+package com.itech.redwine.parser;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.itech.common.util.UtilHttp;
+import com.itech.offer.fetchers.parser.CitiOfferParser;
+import com.itech.offer.fetchers.parser.CommonHttpParser;
+
+public class RedWineCardsParser {
+
+	private final Logger logger = Logger.getLogger(CitiOfferParser.class);
+	private String redWinedomainName ="http://redanar.com";
+	private String redWineBasePageUrl= redWinedomainName+"/mobile/badge/list?search=&page=";
+	private String redWineOffersBaseUrl = redWinedomainName +"/mobile/badge/offers/cardNumber?page=offerPagenumber";
+		
+	private List<RedWineCard> redWineCards = new ArrayList<RedWineCard>();
+	
+	public void printGetRedWineOffers(){
+		parseRedWine();
+		writeRedWineToRecords();
+	}
+	
+	private void writeRedWineToRecords() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void parseRedWine(){
+		for(int currentPageNumber =0; currentPageNumber <= 165; currentPageNumber++){
+			redWineCards.addAll(getRedWineCardsForPage(currentPageNumber));
+		}	
+	}
+
+	private List<RedWineCard> getRedWineCardsForPage(int currentPageNumber) {
+		List<RedWineCard> redWineCards = new ArrayList<RedWineCard>();
+		String currentPageUrl = getBasePageUrl()+currentPageNumber;
+		String respHtml = UtilHttp.fetchHttpResponse(currentPageUrl);
+		Document doc = Jsoup.parse(respHtml);
+		List<Element> cardBlockElements = doc.select("li.clearfix");
+		for(Element cardElement: cardBlockElements){
+			Element cardLinkElement = cardElement.select("div p a").first();
+			RedWineCard rWCard = new RedWineCard();
+			rWCard.setCardName(cardLinkElement.text());
+			rWCard.setCardUrl(cardLinkElement.attr("href"));
+			rWCard.setCardElement(cardElement);
+			setOtherCardDetails(rWCard);
+			getAllOffersForCurrentCard(rWCard);
+			redWineCards.add(rWCard);
+		}
+		return redWineCards;
+	}
+
+	private void setOtherCardDetails(RedWineCard rWCard) {
+		int countOfOffers = getCountOfOffers(rWCard);
+		rWCard.setOffersCount(countOfOffers);
+		String cardOffersUrl = redWinedomainName+rWCard.getCardUrl();
+		String respHtml = UtilHttp.fetchHttpResponse(cardOffersUrl);
+		Document doc = Jsoup.parse(respHtml);
+		String cardType = doc.getElementsContainingOwnText("Card Type").first().nextElementSibling().text();
+		rWCard.setCardType(cardType);
+		String imageSrc =  doc.getElementsContainingOwnText("Card Image").first().nextElementSibling().child(0).attr("src");
+		rWCard.setImageUrl(imageSrc);
+		getAllOffersForCurrentCard(rWCard);
+	}
+
+	private void getAllOffersForCurrentCard(RedWineCard rWCard) {
+		int cardNumber = getCardNumberFromCarUrl(rWCard);
+		for(int pageNumber =0; pageNumber < 10; pageNumber++){
+			rWCard.getOffers().addAll(getCardPageOffers(cardNumber, pageNumber));
+		}
+	}
+
+	private List<RedWineOffer> getCardPageOffers(
+			int cardNumber, int pageNumber) {
+		List<RedWineOffer> redWineOffers = new ArrayList<RedWineOffer>();
+		String offersUrl = redWineOffersBaseUrl.replace("cardNumber", cardNumber+"");
+		offersUrl = offersUrl.replace("offerPageNumber", pageNumber+"");
+		String respHtml = UtilHttp.fetchHttpResponse(offersUrl);
+		if(null != respHtml){
+			Document doc = Jsoup.parse(respHtml);
+			List<Element> offerElements = doc.select("li.clearfix");
+			for(Element offerElement: offerElements){
+				RedWineOffer rOffer = new RedWineOffer();
+				rOffer.setMerchantName(offerElement.child(0).text());
+				rOffer.setDiscriptionn(offerElement.child(1).text());
+				redWineOffers.add(rOffer);
+			}
+		}
+		return redWineOffers;
+	}
+
+	public static void main(String[] args){
+		new RedWineCardsParser().parseRedWine();
+	}
+	
+	private int getCardNumberFromCarUrl(RedWineCard rWCard) {
+		 int length = rWCard.getCardUrl().length();
+		 return Integer.parseInt(rWCard.getCardUrl().substring(length-2, length));
+	}
+
+	private int getCountOfOffers(RedWineCard rWCard) {
+		String[] offerCount = rWCard.getCardElement().text().split("\\|");
+		String offerCountString = offerCount[1];
+		int count = Integer.parseInt(offerCountString.trim().substring(0, offerCountString.length()-8));
+		return count;
+	}
+
+	public List<RedWineCard> getRedWineCards() {
+		return redWineCards;
+	}
+
+	public void setRedWineCards(List<RedWineCard> redWineCards) {
+		this.redWineCards = redWineCards;
+	}
+
+	public String getBasePageUrl() {
+		return redWineBasePageUrl;
+	}
+
+	public void setBasePageUrl(String basePageUrl) {
+		this.redWineBasePageUrl = basePageUrl;
+	}
+	
+}
---
+++ coupmgr/src/com/itech/redwine/parser/RedWineOffer.java
@@ -0,0 +1,27 @@
+package com.itech.redwine.parser;
+
+public class RedWineOffer {
+ 
+ private String merchantName;
+ private String discriptionn;
+ private String expiryDate;
+
+ public String getMerchantName() {
+	return merchantName;
+}
+public void setMerchantName(String merchantName) {
+	this.merchantName = merchantName;
+}
+public String getDiscriptionn() {
+	return discriptionn;
+}
+public void setDiscriptionn(String discriptionn) {
+	this.discriptionn = discriptionn;
+}
+public String getExpiryDate() {
+	return expiryDate;
+}
+public void setExpiryDate(String expiryDate) {
+	this.expiryDate = expiryDate;
+}
+}
