91c99ba1254ebcf328452853fc9d2840248bd9fa
dadua <dadua@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2012-08-09 19:57:08 +0000

    merging changes from coming soon branch to mainline for revisions 197-200.

--- coupmgr/src/com/itech/common/services/CommonBaseManager.java
+++ coupmgr/src/com/itech/common/services/CommonBaseManager.java
@@ -1,9 +1,11 @@
 package com.itech.common.services;
 
+import com.itech.common.db.hibernate.HibernateSessionFactory;
 import com.itech.common.security.SecurityManager;
 import com.itech.coupon.model.User;
 
 public class CommonBaseManager {
+	private HibernateSessionFactory hibernateSessionFactory;
 	private SecurityManager securityManager;
 
 	public User getLoggedInUser() {
@@ -16,6 +18,14 @@ public class CommonBaseManager {
 
 	public SecurityManager getSecurityManager() {
 		return securityManager;
+	}
+
+	public HibernateSessionFactory getHibernateSessionFactory() {
+		return hibernateSessionFactory;
+	}
+
+	public void setHibernateSessionFactory(HibernateSessionFactory hibernateSessionFactory) {
+		this.hibernateSessionFactory = hibernateSessionFactory;
 	}
 
 }
