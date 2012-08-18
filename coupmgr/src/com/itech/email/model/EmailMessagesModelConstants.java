8b789ba22778ef1473b0518418f422fb9819bf2c
bpsingh <bpsingh@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2012-08-18 15:19:43 +0000

    adding db/model for EMAIL_MESSAGES table for async email process

---
+++ coupmgr/src/com/itech/email/model/EmailMessagesModelConstants.java
@@ -0,0 +1,16 @@
+package com.itech.email.model;
+
+public class EmailMessagesModelConstants {
+
+	public static final String TABLE_EMAIL_MESSAGEs="EMAIL_MESSAGES";
+	public static final String COL_ID="ID";
+	public static final String COL_TO_ADDRESS="TO_ADDRESS";
+	public static final String COL_FROM_ADDRESS="FROM_ADDRESS";
+	public static final String COL_MESSAGE_CONTENT="MESSAGE_CONTENT";
+	public static final String COL_SUBJECT="SUBJECT";
+	public static final String COL_CREATION_TIME="CREATION_TIME";
+	public static final String COL_LAST_TRY_TIME="LAST_TRY_TIME";
+	public static final String COL_TRY_COUNT="TRY_COUNT";
+	public static final String COL_STATUS="STATUS";
+	public static final String COL_PRIORITY="PRIORITY";
+}
