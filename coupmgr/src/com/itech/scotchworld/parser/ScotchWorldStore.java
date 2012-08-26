1367c32fde13fcb8e42874425c80c10ad30cd2f3
dadua <dadua@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2012-08-26 05:46:47 +0000

    adding scotch to diary..

---
+++ coupmgr/src/com/itech/scotchworld/parser/ScotchWorldStore.java
@@ -0,0 +1,26 @@
+package com.itech.scotchworld.parser;
+
+public class ScotchWorldStore {
+	private String storeName;
+	private String description;
+	private String imageSrc;
+
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	public String getStoreName() {
+		return storeName;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setImageSrc(String imageSrc) {
+		this.imageSrc = imageSrc;
+	}
+	public String getImageSrc() {
+		return imageSrc;
+	}
+}
