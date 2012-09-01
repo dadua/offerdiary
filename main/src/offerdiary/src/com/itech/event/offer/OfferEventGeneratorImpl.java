8cd8181c0ed892ea3f2cf5c77d5509aa6caef015
dadua <dadua@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2012-09-01 14:42:22 +0000

    commiting pending code from refactoring

---
+++ main/src/offerdiary/src/com/itech/event/offer/OfferEventGeneratorImpl.java
@@ -0,0 +1,37 @@
+package com.itech.event.offer;
+
+import com.itech.event.services.EventEngine;
+import com.itech.event.vo.Event;
+import com.itech.event.vo.OfferEvent;
+import com.itech.event.vo.OfferEvent.OfferEventType;
+import com.itech.offer.model.Offer;
+
+public class OfferEventGeneratorImpl implements OfferEventGenerator {
+	private EventEngine eventEngine;
+
+	@Override
+	public void offerCreated(Offer offer) {
+		Event event = new OfferEvent(OfferEventType.OFFER_CREATED, offer);
+		eventEngine.handleEvent(event);
+	}
+
+	@Override
+	public void offerModified(Offer offer) {
+		Event event = new OfferEvent(OfferEventType.OFFER_MODIFIED, offer);
+		eventEngine.handleEvent(event);
+	}
+
+	@Override
+	public void offerDeleted(Offer offer) {
+		Event event = new OfferEvent(OfferEventType.OFFER_DELETED, offer);
+		eventEngine.handleEvent(event);
+	}
+
+	public void setEventEngine(EventEngine eventEngine) {
+		this.eventEngine = eventEngine;
+	}
+
+	public EventEngine getEventEngine() {
+		return eventEngine;
+	}
+}
