23d1d771bcd5cec8001611415cc15347607c9516
aloksharma <aloksharma@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2013-01-31 03:39:41 +0000

    Adding DOB column to user model and user vo

--- main/src/offerdiary/src/com/itech/user/vos/UserInfoVO.java
+++ main/src/offerdiary/src/com/itech/user/vos/UserInfoVO.java
@@ -1,15 +1,21 @@
 package com.itech.user.vos;
 
+import java.sql.Date;
+
+import com.itech.user.model.LoginType;
+import com.itech.user.model.User;
+
+
 public class UserInfoVO {
 	private String name;
 	private int age;
+	private Long dobMillis;
 	private String city;
 	private String emailId;
 	private String nickname;
 	private String mobileNumber;
 	private String pinCode;
 	private boolean isLinkedUser;
-
 
 	public String getName() {
 		return name;
@@ -58,6 +64,44 @@ public class UserInfoVO {
 	}
 	public void setLinkedUser(boolean isLinkedUser) {
 		this.isLinkedUser = isLinkedUser;
+	}
+
+	public void setDobMillis(Long dobMillis) {
+		this.dobMillis = dobMillis;
+	}
+	public Long getDobMillis() {
+		return dobMillis;
+	}
+
+
+	public static UserInfoVO getUserInfoVOFor(User user) {
+		UserInfoVO userInfoVO = new UserInfoVO();
+		userInfoVO.setAge(user.getAge());
+		userInfoVO.setCity(user.getCity());
+		userInfoVO.setEmailId(user.getEmailId());
+		if (!LoginType.EMAIL.equals(user.getLoginType())) {
+			userInfoVO.setLinkedUser(true);
+		}
+		userInfoVO.setMobileNumber(user.getMobileNumber());
+		userInfoVO.setName(user.getName());
+		userInfoVO.setNickname(user.getNickname());
+		userInfoVO.setPinCode(user.getPinCode());
+		userInfoVO.setDobMillis(user.getDob().getTime());
+		return userInfoVO;
+	}
+
+
+	public static void fillUserFromUserVO(User user, UserInfoVO userInfoVO) {
+		if ((userInfoVO.getDobMillis()!= null) && (userInfoVO.getDobMillis() !=0)) {
+			user.setDob(new Date(userInfoVO.getDobMillis()));
+		}
+		user.setAge(userInfoVO.getAge());
+		user.setCity(userInfoVO.getCity());
+		user.setEmailId(userInfoVO.getEmailId());
+		user.setMobileNumber(userInfoVO.getMobileNumber());
+		user.setName(userInfoVO.getName());
+		user.setNickname(userInfoVO.getNickname());
+		user.setPinCode(userInfoVO.getPinCode());
 	}
 
 
