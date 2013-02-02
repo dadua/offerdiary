a9480ee5378e1cb19c8046209130a2920018e2b8
aloksharma <aloksharma@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2013-02-02 11:05:41 +0000

    One edit button for all user details.. Separating the userinfo functionality into separate js..

--- main/src/offerdiary/WebContent/profile/js/profile.js
+++ main/src/offerdiary/WebContent/profile/js/profile.js
@@ -6,53 +6,9 @@
 var it = it || {};
 it.profile = it.profile || {};
 
-it.profile.view = function(){
-    var _getUserInfo$ = function (userInfo) {
-        var _userInfo$ = $('.user_info_template').clone();
-        userInfo = $.extend({
-            name: '',
-            age: '-',
-            dobMillis: '-',
-            city: '-',
-            emailId: '-',
-            nickName: '-',
-            mobileNumber: '-',
-            pinCode: '-'
-        }, userInfo);
-        _userInfo$.removeClass('user_info_template').addClass('userInfo');
-        _userInfo$.find('.userName').html(userInfo.name);
-        _userInfo$.find('.userCity').html(userInfo.city);
-        _userInfo$.find('.userEmail').html(userInfo.emailId);
-        _userInfo$.find('.dob').html(userInfo.dobMillis);
-        _userInfo$.find('.nickName').html(userInfo.nickName);
-        _userInfo$.find('.pinCode').html(userInfo.pinCode);
-        _userInfo$.find('.mobileNumber').html(userInfo.mobileNumber);
-        return _userInfo$;
-    };
-
-
-    return {
-        getUserInfo$: _getUserInfo$
-    };
-}();
-
-
-it.profile.refreshUserInfo = function() {
-    $.getJSON('getUserInfo.do', function(data){
-        var userInfo = data.result;
-        it.profile.plotUserInfo(userInfo);
-    });
-};
-
-it.profile.plotUserInfo = function(userInfo) {
-    var userInfo$ = this.view.getUserInfo$(userInfo);
-    $('#profileContainer').html(userInfo$);
-
-};
-
 it.profile.toggler = function () {
     var _showUserDetails = function () {
-        
+        it.profile.userInfo.refresh ();
     },
 
     _showNotificationsettings = function () {
