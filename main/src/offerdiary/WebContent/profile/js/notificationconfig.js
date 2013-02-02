d3677230425a1e465ff9ec1898612f498ece6097
aloksharma <aloksharma@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2013-02-02 16:39:05 +0000

    Notification config view edit and save functionality..

---
+++ main/src/offerdiary/WebContent/profile/js/notificationconfig.js
@@ -0,0 +1,105 @@
+var it = it || {};
+it.profile = it.profile || {};
+it.profile.notification= it.profile.notification|| {};
+
+it.profile.notification.view = function () {
+    var _get$ = function (notificationConfigVO) {
+        var _notificationConfig$ = $('.notification_config_template').clone();
+        notificationConfigVO = $.extend({
+            weeklySummary: false,
+            promotions: false,
+            offerExpiryAlert: true,
+            offerExpiryAlertDays: 5
+        }, notificationConfigVO);
+
+        _notificationConfig$.removeClass('notification_config_template').addClass('notificationConfig');
+        _notificationConfig$.find('.weeklySummary').html(notificationConfigVO.weeklySummary?'yes':'no');
+        _notificationConfig$.find('.promotions').html(notificationConfigVO.promotions?'yes':'no');
+        _notificationConfig$.find('.offerExpiryAlert').html(notificationConfigVO.offerExpiryAlert?'yes':'no');
+        _notificationConfig$.find('.offerExpiryAlertDays').html(notificationConfigVO.offerExpiryAlertDays);
+        return _notificationConfig$;
+    };
+
+    var _saveHandler = function () {
+        var notificationConfig$ = $('.notificationConfig'),
+            weeklySummary = notificationConfig$.find('.weeklySummaryCbox').prop('checked'),
+            promotions = notificationConfig$.find('.promotionsCbox').prop('checked'),
+            offerExpiryAlert = notificationConfig$.find('.offerExpiryAlertCbox').prop('checked'),
+            offerExpiryAlertDays = notificationConfig$.find('.offerExpiryAlertDaysTxt').val(),
+            notificationConfigVO = { 
+                weeklySummary: weeklySummary,
+                promotions: promotions,
+                offerExpiryAlert: offerExpiryAlert,
+                offerExpiryAlertDays: offerExpiryAlertDays
+            };
+
+        $.post('updateUserNotificationConfig.do', {userNotificationConfig: JSON.stringify(notificationConfigVO)}, function (data) {
+            it.profile.notification.refresh();
+        });
+
+    };
+
+    var _copyValsToEditCols = function () {
+        $('.editColumn').each(function(){
+            var this$ = $(this),
+                val = this$.parent().find('.valColumn').html();
+
+            var input$ = this$.find('input'),
+                inputType = input$.attr('type');
+            if (inputType == 'text') {
+                input$.val(val); 
+            } else if (inputType == 'checkbox') {
+                if (val === 'yes') {
+                    input$.prop('checked', true);
+                } else {
+                    input$.prop('checked', false);
+                }
+            }
+        });
+
+    };
+
+    var _cancelHandler = function () {
+        $('.editColumn').hide();
+        $('.valColumn').show();
+        $('.saveCancel').addClass('hide');
+        $('.editBtn').show();
+    };
+
+    var _editHandler =  function () {
+        $('.editColumn').show();
+        $('.valColumn').hide();
+        $('.saveCancel').removeClass('hide');
+        $('.editBtn').hide();
+        _copyValsToEditCols();
+    };
+
+
+    var _addHandlers = function () {
+        $('.editBtn').click(_editHandler);
+        $('.saveBtn').click(_saveHandler);
+        $('.cancelBtn').click(_cancelHandler);
+        _cancelHandler();
+    };
+
+
+
+    return {
+        get$: _get$,
+        addHandlers: _addHandlers
+    };
+}();
+
+it.profile.notification.plot = function (notificationConfigVO) {
+    var notificationConfig$ = this.view.get$(notificationConfigVO);
+    $('#profileContainer').html(notificationConfig$);
+    this.view.addHandlers();
+};
+
+it.profile.notification.refresh = function () {
+    var that = this;
+    $.getJSON('getUserNotificationConfig.do', function (data) {
+        var notificationConfigVO = data.result;
+        that.plot (notificationConfigVO);
+    });
+};
--- main/src/offerdiary/WebContent/profile/pages/profile.jsp
+++ main/src/offerdiary/WebContent/profile/pages/profile.jsp
@@ -6,12 +6,13 @@
 	
 	<script src="profile/js/profile.js"></script>
 	<script src="profile/js/userinfo.js"></script>
+	<script src="profile/js/notificationconfig.js"></script>
 	<script type="text/javascript">
 
 		$(function(){
 			$('div.tabbable ul.nav li').removeClass('active');
 			$('#profileTab').addClass('active');
-			it.profile.toggler.showUserDetails();
+			it.profile.init();
 		});
 		
 		
@@ -30,8 +31,8 @@
                 	<div class="container-fluid options-left-container">
 	                	<ul class="nav nav-list">
 			                <li class="nav-header">User:</li>
-							<li class="active"><a href="#">Details</a></li>
-							<li><a href="#">Notification Settings</a></li>
+							<li class="active" id="userDetails"><a href="#">Details</a></li>
+							<li><a href="#" id="notificationSettings">Notification Settings</a></li>
 							<li><a href="#">Points</a></li>
 							<li><a href="#">Linked Accounts</a></li>
 							<li class="divider"></li>
@@ -50,5 +51,6 @@
 		</div>
 		<div class="hide" style="display:none">
 			<%@include file="/profile/templates/userDetails.html" %>
+			<%@include file="/profile/templates/notificationConfig.html" %>
 		</div>
 <%@include file="/common/pages/bodyHtmlEnd.jsp" %>
