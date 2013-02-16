013f37ed885ed7765f34dd192b03bed445ebd4c1
aloksharma <aloksharma@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2013-02-16 13:45:25 +0000

    Moving fb js to another js file..

---
+++ main/src/offerdiary/WebContent/offers/js/share_offers.js
@@ -0,0 +1,16 @@
+var it = it || {};
+it.offer = it.offer || {};
+it.offer.share = it.offer.share || {};
+
+it.offer.share.init = function () {
+
+};
+
+it.offer.share.fb = function () {
+
+};
+
+it.offer.share.email = function () {
+
+};
+
