a3b10ab079fa9a4e59016bd452d3c684810adbfa
dadua <dadua@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2013-02-20 19:42:23 +0000

    support for FB post details in share added.

---
+++ main/src/offerdiary/src/com/itech/offer/vo/FBPostVO.java
@@ -0,0 +1,57 @@
+package com.itech.offer.vo;
+
+public class FBPostVO {
+
+	private String appId;
+	private String redirectURI;
+	private String link;
+	private String pictureURL;
+	private String name;
+	private String caption;
+	private String description;
+
+
+	public String getAppId() {
+		return appId;
+	}
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+	public String getRedirectURI() {
+		return redirectURI;
+	}
+	public void setRedirectURI(String redirectURI) {
+		this.redirectURI = redirectURI;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getPictureURL() {
+		return pictureURL;
+	}
+	public void setPictureURL(String pictureURL) {
+		this.pictureURL = pictureURL;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCaption() {
+		return caption;
+	}
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
