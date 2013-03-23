d50851a485ee03506d328be067a8640c3b1a427a
dadua <dadua@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2013-03-23 06:02:18 +0000

    adding sdk classes

---
+++ main/src/offerdiary/src/com/itech/sdk/annotations/Internal.java
@@ -0,0 +1,16 @@
+package com.itech.sdk.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.METHOD)
+@Documented
+@Inherited
+public @interface Internal {
+
+}
