b53fc3344af496884434204cde13f196e5800f14
dadua <dadua@b1e6d7df-1d6d-4298-be86-6b87e87bbc09>
2013-06-08 06:57:52 +0000

    adding actions for flower relationships

--- main/src/flowers/src/com/itech/web/FlowerAction.java
+++ main/src/flowers/src/com/itech/web/FlowerAction.java
@@ -17,21 +17,21 @@ import com.itech.common.web.action.CommonBeanResponse;
 import com.itech.common.web.action.Forward;
 import com.itech.common.web.action.Response;
 import com.itech.common.web.action.Result;
+import com.itech.flower.model.Customer;
 import com.itech.flower.model.Flower;
+import com.itech.flower.model.Supplier;
 
 
 @Controller
 public class FlowerAction extends CommonAction {
 
-	private static final String FLOWER_PARAM = "flower";
-	private static final String FLOWER_ID_PARAM = "id";
-	private static final String FLOWER_SEARCH_STRING_PARAM = "q";
+
 
 
 	@ActionResponseAnnotation(responseType=Forward.class)
 	@ActionMapping(value="addFlower.do")
 	public Response addFlower (HttpServletRequest req, HttpServletResponse resp) {
-		String flowerJson = req.getParameter(FLOWER_PARAM);
+		String flowerJson = req.getParameter(WebConstatnts.FLOWER_PARAM);
 		Gson gson = new Gson();
 		Type flowerType = new TypeToken<Flower>() { }.getType();
 		Flower flower = gson.fromJson(flowerJson, flowerType);
@@ -47,7 +47,7 @@ public class FlowerAction extends CommonAction {
 	@ActionResponseAnnotation(responseType=CommonBeanResponse.class)
 	@ActionMapping(value="deleteFlower.do")
 	public Response deleteFlower (HttpServletRequest req, HttpServletResponse resp) {
-		String flowerId = req.getParameter(FLOWER_ID_PARAM);
+		String flowerId = req.getParameter(WebConstatnts.FLOWER_ID_PARAM);
 		Flower flower = getFlowerManager().getFlowerById(Long.parseLong(flowerId));
 		getFlowerManager().delete(flower);
 		Result<String> result = new Result<String>("Successfully removed the flower " + flower.getName());
@@ -59,7 +59,7 @@ public class FlowerAction extends CommonAction {
 	@ActionResponseAnnotation(responseType=CommonBeanResponse.class)
 	@ActionMapping(value="getFlowers.do")
 	public Response getFlowers (HttpServletRequest req, HttpServletResponse resp) {
-		String searchString = req.getParameter(FLOWER_SEARCH_STRING_PARAM);
+		String searchString = req.getParameter(WebConstatnts.FLOWER_SEARCH_STRING_PARAM);
 		List<Flower> flowers = getFlowerManager().searchFlowersFor(searchString);
 		Result<List<Flower>> result = new Result<List<Flower>>(flowers);
 		Type resultStringType = new TypeToken<Result<List<Flower>>>() {
@@ -70,13 +70,96 @@ public class FlowerAction extends CommonAction {
 	@ActionResponseAnnotation(responseType=CommonBeanResponse.class)
 	@ActionMapping(value="getFlowerDetails.do")
 	public Response getFlowerDetails (HttpServletRequest req, HttpServletResponse resp) {
-		String flowerId = req.getParameter(FLOWER_ID_PARAM);
+		String flowerId = req.getParameter(WebConstatnts.FLOWER_ID_PARAM);
 		Flower flower = getFlowerManager().getFlowerById(Long.parseLong(flowerId));
 		Result<Flower> result = new Result<Flower>(flower);
 		Type resultStringType = new TypeToken<Result<Flower>>() {
 		}.getType();
 		return new CommonBeanResponse(result, resultStringType);
 	}
+
+
+	@ActionResponseAnnotation(responseType=Forward.class)
+	@ActionMapping(value="addFlowerToCustomer.do")
+	public Response addFlowerToCustomer (HttpServletRequest req, HttpServletResponse resp) {
+		String flowerId = req.getParameter(WebConstatnts.FLOWER_ID_PARAM);
+		String customerId = req.getParameter(WebConstatnts.CUSTOMER_ID_PARAM);
+		Flower flower = getFlowerManager().getFlowerById(Long.parseLong(flowerId));
+		Customer customer = getCustomerManager().getById(Long.parseLong(customerId));
+		getFlowerManager().addFlowerToCustomer(flower, customer);
+		Result<String> result = new Result<String>("Successfully added the flower " + flower.getName() + " to Customer " + customer.getName());
+		Type resultStringType = new TypeToken<Result<String>>() {
+		}.getType();
+		return new CommonBeanResponse(result, resultStringType);
+	}
+
+	@ActionResponseAnnotation(responseType=Forward.class)
+	@ActionMapping(value="addFlowerToSupplier.do")
+	public Response addFlowerToSupplier (HttpServletRequest req, HttpServletResponse resp) {
+		String flowerId = req.getParameter(WebConstatnts.FLOWER_ID_PARAM);
+		String supplierId = req.getParameter(WebConstatnts.SUPPLIER_ID_PARAM);
+		Flower flower = getFlowerManager().getFlowerById(Long.parseLong(flowerId));
+		Supplier supplier = getSupplierManager().getById(Long.parseLong(supplierId));
+		getFlowerManager().addFlowerToSupplier(flower, supplier);
+		Result<String> result = new Result<String>("Successfully added the flower " + flower.getName() + " to Supplier " + supplier.getName());
+		Type resultStringType = new TypeToken<Result<String>>() {
+		}.getType();
+		return new CommonBeanResponse(result, resultStringType);
+	}
+
+	@ActionResponseAnnotation(responseType=CommonBeanResponse.class)
+	@ActionMapping(value="getFlowersForSupplier.do")
+	public Response getFlowersForSupplier (HttpServletRequest req, HttpServletResponse resp) {
+		String supplierId = req.getParameter(WebConstatnts.SUPPLIER_ID_PARAM);
+		Supplier supplier = getSupplierManager().getById(Long.parseLong(supplierId));
+		List<Flower> flowers = getFlowerManager().getFlowersForSupplier(supplier);
+		Result<List<Flower>> result = new Result<List<Flower>>(flowers);
+		Type resultStringType = new TypeToken<Result<List<Flower>>>() {
+		}.getType();
+		return new CommonBeanResponse(result, resultStringType);
+	}
+
+
+	@ActionResponseAnnotation(responseType=CommonBeanResponse.class)
+	@ActionMapping(value="getFlowersForCustomer.do")
+	public Response getFlowersForCustomer (HttpServletRequest req, HttpServletResponse resp) {
+		String customerId = req.getParameter(WebConstatnts.CUSTOMER_ID_PARAM);
+		Customer customer = getCustomerManager().getById(Long.parseLong(customerId));
+		List<Flower> flowers = getFlowerManager().getFlowersForCustomer(customer);
+		Result<List<Flower>> result = new Result<List<Flower>>(flowers);
+		Type resultStringType = new TypeToken<Result<List<Flower>>>() {
+		}.getType();
+		return new CommonBeanResponse(result, resultStringType);
+	}
+
+
+	@ActionResponseAnnotation(responseType=CommonBeanResponse.class)
+	@ActionMapping(value="getSuppliersForFlower.do")
+	public Response getSuppliersForFlower (HttpServletRequest req, HttpServletResponse resp) {
+		String flowerId = req.getParameter(WebConstatnts.FLOWER_ID_PARAM);
+		Flower flower = getFlowerManager().getFlowerById(Long.parseLong(flowerId));
+		List<Supplier> suppliers = getFlowerManager().getSuppliersForFlower(flower);
+		Result<List<Supplier>> result = new Result<List<Supplier>>(suppliers);
+		Type resultStringType = new TypeToken<Result<List<Supplier>>>() {
+		}.getType();
+		return new CommonBeanResponse(result, resultStringType);
+	}
+
+	@ActionResponseAnnotation(responseType=CommonBeanResponse.class)
+	@ActionMapping(value="getCustomersForFlower.do")
+	public Response getCustomersForFlower (HttpServletRequest req, HttpServletResponse resp) {
+		String flowerId = req.getParameter(WebConstatnts.FLOWER_ID_PARAM);
+		Flower flower = getFlowerManager().getFlowerById(Long.parseLong(flowerId));
+		List<Customer> customers = getFlowerManager().getCustomersForFlower(flower);
+		Result<List<Customer>> result = new Result<List<Customer>>(customers);
+		Type resultStringType = new TypeToken<Result<List<Customer>>>() {
+		}.getType();
+		return new CommonBeanResponse(result, resultStringType);
+	}
+
+
+
+
 
 
 }
--- main/src/flowers/src/com/itech/web/WebConstatnts.java
+++ main/src/flowers/src/com/itech/web/WebConstatnts.java
@@ -1,46 +1,20 @@
 package com.itech.web;
 
-import com.itech.config.ProjectConfigs;
 
 
 
 public class WebConstatnts {
-	public static final String CONSUMER_KEY = "orkud2facebook.appspot.com";
-	public static final String CONSUMER_SECRET = "NHH04EkpzhE+cSM+mJ0hgud+";
-	public static final String BASE_URL = "http://localhost:8080/orkut";
-	public static final String CALLBACK_URL = "/orkutCallback.do";
+	public static final String FLOWER_PARAM = "flower";
+	public static final String FLOWER_ID_PARAM = "flower_id";
+	public static final String FLOWER_SEARCH_STRING_PARAM = "q";
+
+	public static final String SUPPLIER_PARAM = "supplier";
+	public static final String SUPPLIER_ID_PARAM = "supplier_id";
+	public static final String SUPPLIER_SEARCH_STRING_PARAM = "q";
 
 
-	public static final String ORKUT_ADAPTER_KEY = "orkutAdapter";
-	public static final String ORKUT_ACCESSPASS_KEY = "accessPass";
-	public static final String EXTERNAL_CALLBACK_URL_KEY = "externalCallback";
-	public static final String FB_ALBUMS_EXPORTED_KEY = "fbAlbumsExported";
-	public static final String EXPORT_JOB_ID_KEY = "exportJobIdKey";
-	public static final String SOCIAL_PORTAL_URL = "http://www.socialbaba.com";
-
-	public static final String STATS_DATA_DUMP_FILE_DEV = "/home/ec2-user/orkut2fb-log";
-
-	public static final String STATS_DATA_DUMP_FILE_TEST = "c:\\orkut\\orkut2fb-log";
-
-
-	public static final String FB_APP_ID_ATTRIBUTE_KEY = "fbAppId";
-
-
-
-	public static final String getOfferDetailPageBaseURL() {
-		return ProjectConfigs.defaultServerUrl.get() + "/getOfferDetail.do?id=";
-	}
-
-
-	public static final String getVarifyEmailPageBaseURL() {
-		return ProjectConfigs.defaultServerUrl.get() + "/verifyEmail.do?code=";
-	}
-
-	public static String getAbsoluteURL( String url) {
-		String absoluteURL = ProjectConfigs.defaultServerUrl.get();
-		absoluteURL += url;
-		return absoluteURL;
-	}
-
+	public static final String CUSTOMER_PARAM = "customer";
+	public static final String CUSTOMER_ID_PARAM = "customer_id";
+	public static final String CUSTOMER_SEARCH_STRING_PARAM = "q";
 
 }
